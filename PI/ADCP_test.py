363f0cb47f984b70175334d514ee812979246949
janehwu <detectivejane@gmail.com>
2018-06-26 20:21:38 -0700

    Adding helper functions

--- PI/ADCP_test.py
+++ PI/ADCP_test.py
@@ -1,18 +1,51 @@
 import serial
 import time
 
-# COM_PORT = "/dev/ttyUSB0" #tbd
+# COM_PORT = "/dev/ttyUSB0" #Pi
 COM_PORT = "/dev/tty.usbserial-FT8VW9AR" # for John's macbook
 BAUD_RATE = "115200"
 ser = serial.Serial(COM_PORT, BAUD_RATE, stopbits=serial.STOPBITS_ONE)
 
-
+'''
+Setup connection
+'''
 def setup():
     print("Starting ADCP communication")
+    ser = serial.Serial(COM_PORT, BAUD_RATE, stopbits=serial.STOPBITS_ONE)
     ser.write(b'+++')
-    time.sleep(1)
-    s = ser.read(100)
-    print('Message: ', s.decode('utf-8'))
+    s = read_response(ser)
+    #print('Startup message: ', s)
+    return ser
+
+'''
+Sends specified command and outputs response
+
+Inputs:
+    ser - Serial port
+    command - in bytes
+'''
+def send(ser, command):
+    ser.write(command + b'\r')
+    s = read_response(ser, verbose=True)
+    return s
+
+'''
+Reads entire output message
+'''
+def read_response(port, verbose=False):
+    response = b''
+    cur_line = b''
+    while True:
+        s = port.read()
+        response += s
+        cur_line += s
+        if verbose and s == b'\n':
+            print(cur_line[:-2])
+            cur_line = b''
+
+        if b'>' in response: #stop character
+            return response
+    return
 
 def start_ping():
 	print("Requesting Pings")
