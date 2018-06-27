642d8b17817f6188b4db45fb906fd0c0119e8e65
chiyenLee <johlee@g.hmc.edu>
2018-06-27 13:21:38 -0700

    restructuring code

--- PI/ADCP_test.py
+++ PI/ADCP_test.py
@@ -1,17 +1,42 @@
 import serial
+import time
 
-COM_PORT = "/dev/ttyUSB0" #tbd
+# COM_PORT = "/dev/ttyUSB0" #tbd
+COM_PORT = "/dev/tty.usbserial-FT8VW9AR" # for John's macbook
 BAUD_RATE = "115200"
+ser = serial.Serial(COM_PORT, BAUD_RATE, stopbits=serial.STOPBITS_ONE)
+
 
 def setup():
     print("Starting ADCP communication")
-    ser = serial.Serial(COM_PORT, BAUD_RATE, stopbits=serial.STOPBITS_ONE)
     ser.write(b'+++')
-    s = ser.read(80)
-    print('Message: ', s)
+    time.sleep(1)
+    s = ser.read(100)
+    print('Message: ', s.decode('utf-8'))
+
+def start_ping():
+	print("Requesting Pings")
+	ser.write(b'CS\r\n')
+
+def stop_ping():
+	print('Stopping Pings')
+	ser.write(b'CSTOP\r\n')
+	# s = ser.read(10)
+	# time.sleep(1)
+	# print(s)
 
 def main():
     setup()
+    start_ping()
+    while True:
+    	try:
+    		if ser.in_waiting > 0:
+    			data = ser.readline()
+    			print(data)
+    	except KeyboardInterrupt:
+    		break
+    stop_ping()
+    ser.close()
 
 if __name__=='__main__':
     main()
