39d90baf630a0e791aaefc2688ce69f62b540eaf
Jane Wu <jhwu@hmc.edu>
2018-06-27 17:59:26 -0700

    Spaces

--- PI/ADCP_test.py
+++ PI/ADCP_test.py
@@ -16,18 +16,18 @@ boat_xbee = XBeeDevice(xbee_port, 9600)
 Discover the central xbee
 
 Inputs:
-	xbee - XBeeDevice
+    xbee - XBeeDevice
 Outputs:
-	central_xbee - XBeeDevice (None if no discovery)
+    central_xbee - XBeeDevice (None if no discovery)
 '''
 def discover_xbee(xbee):
-	boat_xbee.open()
-	xbee_network = xbee.get_network()
-	xbee_network.start_discovery_process()
-	while xbee_network.is_discovery_running():
-		time.sleep(0.5)
-	central_xbee = xbee_network.discover_device('central')
-	return central_xbee
+    boat_xbee.open()
+    xbee_network = xbee.get_network()
+    xbee_network.start_discovery_process()
+    while xbee_network.is_discovery_running():
+        time.sleep(0.5)
+    central_xbee = xbee_network.discover_device('central')
+    return central_xbee
 
 
 ###############################################################################
@@ -37,100 +37,103 @@ def discover_xbee(xbee):
 Setup ADCP connection
 '''
 def setup():
-	print("Starting ADCP communication")
-	ser.write(b'+++')
-	time.sleep(3)
-	s = read_response()
-	print('Startup message: ', s)
-	return ser
+    print("Starting ADCP communication")
+    ser.write(b'+++')
+    time.sleep(3)
+    s = read_response()
+    print('Startup message: ', s)
+    return ser
 
 '''
 Sends specified command and outputs response
 
 Inputs:
-	ser - Serial port
-	command - in bytes
+    ser - Serial port
+    command - in bytes
 '''
 def send(command):
-	ser.write(command + b'\r\n')
-	s = read_response(ser, verbose=True)
-	return s
+    ser.write(command + b'\r\n')
+    s = read_response(ser, verbose=True)
+    return s
 
 '''
 Reads entire output message
 '''
 def read_response(verbose=False):
-	response = b''
-	cur_line = b''
-	while True:
-		s = ser.read()
-		response += s
-		cur_line += s
-		if verbose and s == b'\n':
-			print(cur_line)
-			cur_line = b''
+    response = b''
+    cur_line = b''
+    while True:
+        s = ser.read()
+        response += s
+        cur_line += s
+        if verbose and s == b'\n':
+            print(cur_line)
+            cur_line = b''
 
-		if b'>' in response: #stop character
-			return response
-	return
+        if b'>' in response: #stop character
+            return response
+    return
 
 ###############################################################################
 # Control Commands
 ###############################################################################
 def start_ping():
-	print("Requesting Pings")
-	ser.write(b'CS\r\n')
-	ser.read(10) #response includes command    
+    print("Requesting Pings")
+    ser.write(b'CS\r\n')
+    ser.read(10) #response includes command    
 
 def stop_ping():
-	print('Stopping Pings')
-	ser.write(b'CSTOP\r\n')
-	
+    print('Stopping Pings')
+    ser.write(b'CSTOP\r\n')
+    
 def read_ensemble(verbose=False):
-	header = ser.read(2)
-	if header != b'\x7f\x7f':
-		print('ERROR no header: ', header)
+    header = ser.read(2)
+    if header != b'\x7f\x7f':
+        print('ERROR no header: ', header)
   
-	num_bytes = ser.read(2)
-	bytes_to_checksum = int.from_bytes(num_bytes, byteorder='little')-4
-	if verbose:
-		print('Num: ', bytes_to_checksum)
-	
-	data = ser.read(bytes_to_checksum)
-	if verbose:
-		print('Data: ', data)
+    num_bytes = ser.read(2)
+    bytes_to_checksum = int.from_bytes(num_bytes, byteorder='little')-4
+    if verbose:
+        print('Num: ', bytes_to_checksum)
+    
+    data = ser.read(bytes_to_checksum)
+    if verbose:
+        print('Data: ', data)
 
-	#use checksum to verify no errors
-	checksum = ser.read(2)
-	checksum_int = int.from_bytes(checksum, byteorder='little') 
-	datasum_int = sum(b'\x7f\x7f' + num_bytes + data) % 2**16
+    #use checksum to verify no errors
+    checksum = ser.read(2)
+    checksum_int = int.from_bytes(checksum, byteorder='little') 
+    datasum_int = sum(b'\x7f\x7f' + num_bytes + data) % 2**16
 
-	if checksum_int != datasum_int:
-		print('ERROR: ', checksum_int, datasum_int)
+    if checksum_int != datasum_int:
+        print('ERROR: ', checksum_int, datasum_int)
 
-	return data
+    return data
 
 ###############################################################################
 
 def main():
-	setup()
-	start_ping()
-	central_xbee = discover_xbee(boat_xbee)
-	
-	if central_xbee = None:
-		print("No device")
-	else:
-		msg = central_xbee.read_data()
-		print(msg.data)
+    setup()
+    central_xbee = discover_xbee(boat_xbee)
+    
+    if central_xbee == None:
+        print("No device")
+        ser.close()
+        boat_xbee.close()
+        return
+    else:
+        msg = central_xbee.read_data()
+        print(msg.data)
 
-	for i in range(2):
-		ensemble = read_ensemble(verbose=True)
-		boat_xbee.send_data_async(central_xbee, ensemble)
-	
-	stop_ping()
-	ser.write(b'===')
-	ser.close()
-	boat_xbee.close()
+    start_ping()
+    for i in range(2):
+        ensemble = read_ensemble(verbose=True)
+        boat_xbee.send_data_async(central_xbee, ensemble)
+    
+    stop_ping()
+    ser.write(b'===')
+    ser.close()
+    boat_xbee.close()
 
 if __name__=='__main__':
-	main()
+    main()
