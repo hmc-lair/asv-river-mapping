0904fa0128d596f6b179c4edad5521650f1c63f4
Jane Wu <jhwu@hmc.edu>
2018-06-28 17:00:21 -0700

    Reorganize file

--- PI/ADCP_readbytes.py
+++ PI/ADCP_readbytes.py
@@ -1,30 +1,38 @@
-import binascii
-
 def main():
-    #read file
+    # READ FILE
     with open('data/test_results.bin','rb') as f:
-        result = b''
+        all_data = b''
         for line in f.readlines():
             data = line
-            result += data 
-    print(result)
+            all_data += data 
+    print(all_data)
+
+    num_types = all_data[5]
     
-    #num data types
-    print(result[5])
-    num_types = result[5]
-    
-    #OFFSETS
+    # OFFSETS
+    # 1. Fix Leader
+    # 2. Variable Leader
+    # 3. Velocity Profile
+    # 6. Bottom Track
+    # 9. Vertical Beam Range
+    # 10-13. GPS
     offsets = []
     for i in range(num_types):
-        offset = result[6+2*i:8+2*i]
+        offset = all_data[6+2*i:8+2*i]
         offset_int = int.from_bytes(offset, byteorder='little')
         offsets.append(offset_int)
     print('Offsets: ', offsets)
-    
-    #FIXED LEADER DATA
-    print([result[x:x+2] for x in offsets])
-    #fixed_leader_offset = offsets[0]
-    #fixed_leader_header = result[fixed_leader_offset:fixed_leader_offset+2]
-    #print('Header: ', fixed_leader_header)
-    #print(int.from_bytes(header, byteorder='little'))
-main()
+    print('Data IDs: ', [all_data[x:x+2] for x in offsets])
+
+    # VARIABLE LEADER
+
+    # VELOCITY PROFILE
+
+    # BOTTOM TRACK
+
+    # VERTICAL BEAM RANGE
+
+
+
+if __name__ == '__main__':
+    main()
