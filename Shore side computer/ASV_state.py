04ced49ee1b7a156aa2eb626f0240045b569b7b5
chiyenLee <johlee@g.hmc.edu>
2018-07-13 16:45:59 -0700

    Reprganize the code

--- Shore side computer/ASV_state.py
+++
@@ -1,9 +0,0 @@
-class ASV_state:
-
-	def __init__(self, lat = 0.0, lon = 0.0, heading = 0.0):
-		self.set_state(lat, lon, heading)
-
-	def set_state(self, lat, lon, heading):
-		self.lat = lat
-		self.lon = lon
-		self.heading = heading
