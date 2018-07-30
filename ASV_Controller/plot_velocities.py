8035bcb965a72d18a4b616c3548b6a73b97a8dea
chiyenLee <johlee@g.hmc.edu>
2018-07-30 14:19:02 -0700

    processing velocity

--- ASV_Controller/plot_velocities.py
+++ ASV_Controller/plot_velocities.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import matplotlib.image as mpimg
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
@@ -11,7 +12,7 @@ from read_asv_data import read_ensemble
 
 # Plot parameters
 CELL_RES = 0.5
-win = 5
+win = 1
 sigma_slope = 0.1204
 sigma_offset = 0.6142
 
@@ -80,6 +81,7 @@ def main():
     # img, inv_trans, geo_trans, MAP_WIDTH, MAP_HEIGHT = load_map(map_file)
     #GPS DATA
 
+    print("Reading data...")
     # Note: Velocities are in millimeters per second
     ASV_X, ASV_Y, Z, depth_cell_length, relative_velocities, bt_velocties = read_data_file(data_file)
 
@@ -163,22 +165,22 @@ def main():
 
 
     # 3) Velocity Surface Map
-    bad_bt_data = [] # flags to signify bag data
-    bad_rel_data = []
-    for i in range(len(relative_velocities)):
-        # if robot is moving at greater than 10 meters/s 
-        if np.linalg.norm(bt_velocties[i]) > 10000:
-            bad_bt_data.append(True)
+    # bad_bt_data = [] # flags to signify bag data
+    # bad_rel_data = []
+    # for i in range(len(relative_velocities)):
+    #     # if robot is moving at greater than 10 meters/s 
+    #     if np.linalg.norm(bt_velocties[i]) > 10000:
+    #         bad_bt_data.append(True)
 
-        else:
-            bad_bt_data.append(False)
-        bad_rel_data.append([])
-        for j in range(len(relative_velocities[i])):
-            if np.linalg.norm(relative_velocities[i][j]) > 30000:
-                bad_rel_data[i].append(True)
-            else:
-                bad_rel_data[i].append(False)
-    print(bad_rel_data)
+    #     else:
+    #         bad_bt_data.append(False)
+    #     bad_rel_data.append([])
+    #     for j in range(len(relative_velocities[i])):
+    #         if np.linalg.norm(relative_velocities[i][j]) > 30000:
+    #             bad_rel_data[i].append(True)
+    #         else:
+    #             bad_rel_data[i].append(False)
+    # print(bad_rel_data)
 
     ### RAW Surface Velocities
     # Relative Surface Velocities
@@ -191,6 +193,10 @@ def main():
     v_boat = bt_velocties
     vx_boat = np.asarray([v[0] for v in v_boat])
     vy_boat = np.asarray([v[1] for v in v_boat])
+
+    # Absolute Velocities
+    vx_sur_abs = vx_sur_rel - vx_boat
+    vy_sur_abs = vy_sur_rel - vy_boat
 
 
     ### Kalman Filtered Surface Velocities
@@ -213,11 +219,15 @@ def main():
     B_vy = vy_current_min*np.ones((m,n))
     B_vx_var = np.zeros((m,n))
     B_vy_var = np.zeros((m,n))
+    win = 2
+    # CELL_RES = 0.1
 
     for t in range(len(ASV_X)):
+        # print("Progress: %d out of %d " %(t, len(ASV_X)))
         cur_x = X[t]
         cur_y = Y[t]
-        cur_alt = Z[t]
+        cur_vx = vx_sur_abs[t]
+        cur_vy = vy_sur_abs[t]
 
         i = int(np.floor(cur_x/CELL_RES)) # find which cell we're in
         j = int(np.floor(cur_y/CELL_RES))
@@ -230,21 +240,57 @@ def main():
                 
                 # if this is first time updating this cell...
                 # set the cell value to current readings
-                if B[k][l] == baseFloor:
-                    B_vx[k][l] = cur_alt
-                    # Calcualte error 
-                    B_vx_var[k][l] = (dist2*sigma_slope+sigma_offset)**2
-                    B_vy_var[k][l] = (dist2*sigma_slope+sigma_offset)**2
+                if B_vx[k][l] == vx_current_min:
+                    if abs(cur_vx) > 1000:
+                        continue
+                    elif abs(cur_vy) > 1000:
+                        continue
+                        B_vx[k][l] = cur_vx
+                        B_vy[k][l] = cur_vy
+
+                        # Calcualte error (related to distance)
+                        B_vx_var[k][l] = (dist2*sigma_slope+sigma_offset)**2
+                        B_vy_var[k][l] = (dist2*sigma_slope+sigma_offset)**2
                 else:
-                    var = (dist2*sigma_slope+sigma_offset)**2
-                    cur_K = float(Bvar[k][l])/(Bvar[k][l] + var)
-                    B[k][l] = B[k][l]+cur_K*(cur_alt - B[k][l])
-                    Bvar[k][l] = Bvar[k][l]-cur_K*Bvar[k][l];
+                    if abs(cur_vx) > 1000:
+                        continue
+                    elif abs(cur_vy) > 1000:
+                        continue
+                    else:
+                        # Current Error for the cell
+                        var = (dist2*sigma_slope+sigma_offset)**2
+                        cur_Kx = float(B_vx_var[k][l])/(B_vx_var[k][l] + var)
+                        cur_Ky = float(B_vy_var[k][l])/(B_vy_var[k][l] + var)
 
+                        B_vx[k][l] = B_vx[k][l]+cur_Kx*(cur_vx - B_vx[k][l])
+                        B_vy[k][l] = B_vy[k][l]+cur_Ky*(cur_vy - B_vy[k][l])
+                        B_vx_var[k][l] = B_vx_var[k][l]-cur_Kx*B_vx_var[k][l]
+                        B_vy_var[k][l] = B_vy_var[k][l]-cur_Ky*B_vy_var[k][l]
 
+    X_plot, Y_plot = np.meshgrid(np.arange(min_x, min_x + m*CELL_RES, CELL_RES), np.arange(min_y, min_y + n*CELL_RES, CELL_RES))
+    
+    # Flip
+    B_vx_new = np.zeros((n,m))
+    B_vy_new = np.zeros((n,m))
+    for i in range(n):
+        for j in range(m):
+            B_vx_new[i][j] = B_vx[j][i]
+            B_vy_new[i][j] = B_vy[j][i]
+    # print(X_plot)
     # Plotting
     # plt.figure()
-    # plt.quiver(ASV_X, ASV_Y, vx_sur_rel, vy_sur_rel)
+    # print(max(B_vx), min(B_vy))
+    # print(B_vx[1,:])
+
+    norm = matplotlib.colors.Normalize()
+    B_speed = np.sqrt(B_vx_new**2 + B_vy_new**2)
+    norm.autoscale(B_speed)
+    cm = matplotlib.cm.copper
+    sm = matplotlib.cm.ScalarMappable(cmap=cm, norm=norm)
+    sm.set_array([])
+
+    plt.quiver(X_plot, Y_plot, B_vx_new, B_vy_new, color=cm(norm(B_speed)))
+    plt.colorbar(sm)
     # plt.title('Relative Surface Velocities')
 
     # plt.figure()
@@ -252,12 +298,12 @@ def main():
     # plt.title('Boat Velocities')
 
     # plt.figure()
-    # Q = plt.quiver(ASV_X, ASV_Y, vx_sur_rel - vx_boat, vy_sur_rel - vy_boat, pivot='tip', scale=1000)
+    # Q = plt.quiver(X_plot, Y_plot, B_vx, vy_sur_rel - vy_boat, pivot='tip', scale=1000)
     # qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
     #                coordinates='figure')
     # plt.scatter(ASV_X, ASV_Y, color='k', s=5)
-    # plt.title('Absolute Velocities')
-    # plt.show()
+    plt.title('Absolute Velocities')
+    plt.show()
 
 if __name__ == '__main__':
     main()
