8c63b064b627f64993ab67aa2d86be0279ea4154
Jane Wu <jhwu@hmc.edu>
2018-08-24 15:59:52 -0700

    Reverted bathymetry plot

--- ASV_Controller/plot_bathymetry.py
+++ ASV_Controller/plot_bathymetry.py
@@ -146,10 +146,6 @@ def get_track_errs(ASV_nor, ASV_eas, wp_nor, wp_eas, mission_nor, mission_eas):
             wps_nor.append(wp_nor[i])
         if wp_eas[i] != 1.0 and wp_eas[i] not in wps_eas:
             wps_eas.append(wp_eas[i])
-    print(wps_nor)
-    print(wps_eas)
-    print(mission_nor)
-    print(wps_eas)
     return errs
 
 #Read data from 1 trial
@@ -188,43 +184,43 @@ def main():
     #     avg += dist
     # print('Avg error: ', avg/num)
 
-    # min_depth = Z.min()
+    min_depth = Z.min()
 
-    # #Normalize positions
-    # min_x = min(ASV_nor)
-    # min_y = min(ASV_eas)
-    # X = [v - min_x for v in ASV_nor]
-    # Y = [v - min_y for v in ASV_eas]
-    # max_x = max(X)
-    # max_y = max(Y)
+    #Normalize positions
+    min_x = min(ASV_nor)
+    min_y = min(ASV_eas)
+    X = [v - min_x for v in ASV_nor]
+    Y = [v - min_y for v in ASV_eas]
+    max_x = max(X)
+    max_y = max(Y)
 
-    # # Method 0: 1D Kalman Filter
-    # m = int(np.ceil(max_x/CELL_RES))
-    # n = int(np.ceil(max_y/CELL_RES))
-    # baseFloor = min_depth
-    # B = baseFloor*np.ones((m,n))
-    # Bvar = np.zeros((m,n))
+    # Method 0: 1D Kalman Filter
+    m = int(np.ceil(max_x/CELL_RES))
+    n = int(np.ceil(max_y/CELL_RES))
+    baseFloor = min_depth
+    B = baseFloor*np.ones((m,n))
+    Bvar = np.zeros((m,n))
 
-    # for t in range(len(ASV_nor)):
-    #     cur_x = X[t]
-    #     cur_y = Y[t]
-    #     cur_alt = Z[t]
+    for t in range(len(ASV_nor)):
+        cur_x = X[t]
+        cur_y = Y[t]
+        cur_alt = Z[t]
 
-    #     i = int(np.floor(cur_x/CELL_RES))
-    #     j = int(np.floor(cur_y/CELL_RES))
+        i = int(np.floor(cur_x/CELL_RES))
+        j = int(np.floor(cur_y/CELL_RES))
 
-    #     for k in range(max(0,i-win), min(m, i+win)):
-    #         for l in range(max(0,j-win), min(n,j+win)):
-    #             dist2 = 0.1+CELL_RES*((k-i)**2+(l-j)**2)**0.5
+        for k in range(max(0,i-win), min(m, i+win)):
+            for l in range(max(0,j-win), min(n,j+win)):
+                dist2 = 0.1+CELL_RES*((k-i)**2+(l-j)**2)**0.5
                     
-    #             if B[k][l] == baseFloor:
-    #                 B[k][l] = cur_alt
-    #                 Bvar[k][l] = (dist2*sigma_slope+sigma_offset)**2
-    #             else:
-    #                 var = (dist2*sigma_slope+sigma_offset)**2
-    #                 cur_K = float(Bvar[k][l])/(Bvar[k][l] + var)
-    #                 B[k][l] = B[k][l]+cur_K*(cur_alt - B[k][l])
-    #                 Bvar[k][l] = Bvar[k][l]-cur_K*Bvar[k][l];
+                if B[k][l] == baseFloor:
+                    B[k][l] = cur_alt
+                    Bvar[k][l] = (dist2*sigma_slope+sigma_offset)**2
+                else:
+                    var = (dist2*sigma_slope+sigma_offset)**2
+                    cur_K = float(Bvar[k][l])/(Bvar[k][l] + var)
+                    B[k][l] = B[k][l]+cur_K*(cur_alt - B[k][l])
+                    Bvar[k][l] = Bvar[k][l]-cur_K*Bvar[k][l];
 
     ###########################################################################
     # PLOTS
@@ -240,28 +236,28 @@ def main():
     plt.plot(mission_X, mission_Y, color='red', marker='.', label='Mission plan')
     plt.legend()
 
-    # X_plot, Y_plot = np.meshgrid(np.arange(min_y, min_y + n*CELL_RES, CELL_RES), np.arange(min_x, min_x + m*CELL_RES, CELL_RES))
+    X_plot, Y_plot = np.meshgrid(np.arange(min_y, min_y + n*CELL_RES, CELL_RES), np.arange(min_x, min_x + m*CELL_RES, CELL_RES))
     
-    # # # 2) Depth surface map
-    # ax1 = plt.figure(figsize=(8,6)).gca(projection='3d')
-    # ax1.plot_surface(X_plot, Y_plot, B, cmap=cm.viridis) #depths
-    # ax1.plot(ASV_eas, ASV_nor, np.zeros(len(X)), color='red') #ASV path
-    # ax1.view_init(200, -50)
-    # ax1.set_zlabel('Depth (m)')
-    # ax1.invert_zaxis()
-    # ax1.set_xlabel('Easting (m)')
-    # ax1.set_ylabel('Northing (m)')
+    # 2) Depth surface map
+    ax1 = plt.figure(figsize=(8,6)).gca(projection='3d')
+    ax1.plot_surface(X_plot, Y_plot, B, cmap=cm.viridis) #depths
+    ax1.plot(ASV_eas, ASV_nor, np.zeros(len(X)), color='red') #ASV path
+    ax1.view_init(200, -50)
+    ax1.set_zlabel('Depth (m)')
+    ax1.invert_zaxis()
+    ax1.set_xlabel('Easting (m)')
+    ax1.set_ylabel('Northing (m)')
 
-    # # # 3) Scatter plot of raw data
-    # ax2 = plt.figure(figsize=(8,6)).gca(projection='3d')
+    # 3) Scatter plot of raw data
+    ax2 = plt.figure(figsize=(8,6)).gca(projection='3d')
 
-    # ax2.scatter(ASV_eas, ASV_nor, Z, c=Z, cmap=cm.viridis)
-    # ax2.plot(ASV_eas, ASV_nor, np.zeros(len(X)), color='red') #ASV path
-    # ax2.view_init(200, -50)
-    # ax2.set_zlabel('Depth (m)')
-    # ax2.invert_zaxis()
-    # ax2.set_xlabel('Easting (m)')
-    # ax2.set_ylabel('Northing (m)')
+    ax2.scatter(ASV_eas, ASV_nor, Z, c=Z, cmap=cm.viridis)
+    ax2.plot(ASV_eas, ASV_nor, np.zeros(len(X)), color='red') #ASV path
+    ax2.view_init(200, -50)
+    ax2.set_zlabel('Depth (m)')
+    ax2.invert_zaxis()
+    ax2.set_xlabel('Easting (m)')
+    ax2.set_ylabel('Northing (m)')
 
     # 4) Gradient plot
     # Gx, Gy = np.gradient(B) # gradients with respect to x and y
